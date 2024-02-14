efac9172fe6f1f2b65bc3e9894f2a46cea8d21e2
K.Nacer-eddine <k.nacereddine1@gmail.com>
2024-02-14 17:57:56 +0100

    Declared the passwords app in the django settings and added the passwords model

--- backend/PWM/passwords/models.py
+++ backend/PWM/passwords/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+class Passwords(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    app_name = models.CharField(max_length=50)
+    link = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    generated_password = models.CharField(max_length=255)
