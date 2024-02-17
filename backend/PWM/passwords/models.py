3668cad33d8d591f331c2398a53c4ed81ec7cd33
K.Nacer-eddine <k.nacereddine1@gmail.com>
2024-02-17 13:07:31 +0100

    Changesd the model name to password and added 'title' field to it

--- backend/PWM/passwords/models.py
+++ backend/PWM/passwords/models.py
@@ -3,6 +3,7 @@ from users.models import User
 # Create your models here.
 class Passwords(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, null=True)
     app_name = models.CharField(max_length=50)
     link = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
