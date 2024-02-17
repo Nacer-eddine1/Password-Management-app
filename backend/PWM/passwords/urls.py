bee909a844b7c44f1250e1037e3e92759bc0196e
K.Nacer-eddine <k.nacereddine1@gmail.com>
2024-02-17 17:11:13 +0100

    modified the passwords app urls.py file

--- backend/PWM/passwords/urls.py
+++ backend/PWM/passwords/urls.py
@@ -4,14 +4,14 @@ from .views import get_passwords, create_password, update_password, delete_passw
 # Define URL patterns for passwords-related views
 urlpatterns = [
     # Endpoint to create a new password
-    path('api/passwords/', create_password, name='create_password'),
+    path('passwords/create/<int:user_id>/', create_password, name='create_password'),
 
     # Endpoint to retrieve all passwords for a specific user
-    path('api/passwords/<int:user_id>/', get_passwords, name='get_passwords'),
+    path('passwords/<int:user_id>/', get_passwords, name='get_passwords'),
 
     # Endpoint to update a specific password
-    path('api/passwords/<int:pk>/', update_password, name='update_password'),
+    path('passwords/update/<int:password_id>/', update_password, name='update_password'),
 
     # Endpoint to delete a specific password
-    path('api/passwords/<int:pk>/', delete_password, name='delete_password'),
+    path('passwords/delete/<int:pk>/', delete_password, name='delete_password'),
 ]
